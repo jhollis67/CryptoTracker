4573ce55a859f8164c46792bbdf31e45c498a020
jhollis67 <jhollis67@me.com>
2018-08-15 21:23:43 -0500

    Add networth to CryptoTableViewController. Add respective coin amounts to crypto coins tableview cells.

--- CryptoTracker/CryptoTableViewController.swift
+++ CryptoTracker/CryptoTableViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+private let headerHeight : CGFloat = 100.0
+private let networthHeight : CGFloat = 45.0
+
+
 class CryptoTableViewController: UITableViewController, CoinDataDelegate {
+    
+    var amountLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +27,43 @@ class CryptoTableViewController: UITableViewController, CoinDataDelegate {
     override func viewWillAppear(_ animated: Bool) {
         CoinData.shared.delegate = self
         tableView.reloadData()
+        displayNetWorth()
     }
     
     func newPrices() {
         tableView.reloadData()
+        displayNetWorth()
+    }
+    
+    func createHeaderView() -> UIView {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: headerHeight))
+        headerView.backgroundColor = UIColor.white
+        let networthLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: networthHeight))
+        networthLabel.text = "My Crypto Net Worth"
+        networthLabel.textAlignment = .center
+        headerView.addSubview(networthLabel)
+        
+        amountLabel.frame = CGRect(x: 0, y: networthHeight, width: view.frame.size.width, height: headerHeight - networthHeight)
+        amountLabel.textAlignment = .center
+        amountLabel.font = UIFont.boldSystemFont(ofSize: 60.0)
+        headerView.addSubview(amountLabel)
+        
+        displayNetWorth()
+        
+        return headerView
+    }
+    
+    func displayNetWorth() {
+        amountLabel.text = CoinData.shared.netWorthAsString()
     }
 
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return headerHeight
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return createHeaderView()
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
@@ -39,8 +76,13 @@ class CryptoTableViewController: UITableViewController, CoinDataDelegate {
         
         let coin = CoinData.shared.coins[indexPath.row]
         
-        cell.textLabel?.text = "\(coin.symbol)  -  \(coin.priceAsString())"
+        if coin.amount != 0.0 {
+            cell.textLabel?.text = "\(coin.symbol)  -  \(coin.priceAsString())   -   \(coin.amount)"
+        } else {
+            cell.textLabel?.text = "\(coin.symbol)  -  \(coin.priceAsString())"
+        }
         cell.imageView?.image = coin.image
+
 
          // Configure the cell...
 
